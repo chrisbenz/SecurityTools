84628f930f4ea7f4e5588785b66747627d917593
chrisbenz <cpbenz@protonmail.com>
2020-09-22 23:57:27 -0700

    Test commit

--- ImageRip/imageRip.py
+++ ImageRip/imageRip.py
@@ -5,14 +5,14 @@ import sys
 def main():
 
 	if len(sys.argv) != 2:
-		print("Please specify an image name in your directory for ImageRip to extract data for.")
+		print("""Usage: python3 imageRip.py <Image File Name>""")
 		return
 
 	imageName = sys.argv[1]
 	try:
 		image = Image.open(imageName)
 	except FileNotFoundError:
-		print(imageName + ' could not be opened. Please specify a valid image.')
+		print(imageName + ' could not be opened. Please specify a valid image path.')
 		return 
 
 	print(image)
