f75edca195c1264d7c304803cde7419aced1a43f
chrisbenz <cpbenz@protonmail.com>
2020-09-24 23:51:56 -0700

    Updated README, added kaliQuick script

--- KaliQuick/README.md
+++ KaliQuick/README.md
@@ -2,3 +2,8 @@
 Bash script for downloading some tools I like to have upon loading a new Kali installation. 
 
 ## Tools Included
+- Sublime Text
+- pip3, pip
+- python setuptools
+- gau (get all urls)
+- Sublist3r
---
+++ KaliQuick/kaliQuick.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Sublime Text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get -y update
+sudo apt-get install -y sublime-text
+
+# Pip, Pip3
+sudo apt-get -y install python3-pip
+sudo apt-get -y install python-pip
+
+# Python Setup Tools
+sudo apt-get install -y python3-setuptools
+sudo apt-get install -y python-setuptools
+
+mkdir ~/DownloadedTools
+cd ~/DownloadedTools
+
+echo "Installing gau"
+git clone https://github.com/lc/gau.git
+echo "Complete"
+
+echo "Installing Sublist3r"
+git clone https://github.com/aboul3la/Sublist3r.git
+cd Sublist3r
+sudo pip install -r requirements.txt
+cd ~/DownloadedTools
+echo "Complete"
