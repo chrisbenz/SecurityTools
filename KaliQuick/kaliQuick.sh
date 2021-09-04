c42806015ec34e58d3f93a7219c5308854eceb94
chrisbenz <cpbenz@protonmail.com>
2021-09-03 22:28:42 -0700

    adding subdomainzer

--- KaliQuick/kaliQuick.sh
+++ KaliQuick/kaliQuick.sh
@@ -52,6 +52,13 @@ cd dirsearch
 sudo pip3 install -r requirements.txt
 echo "Complete"
 
+cd ~/DownloadedTools
+echo "Installing SubDomainizer"
+git clone https://github.com/nsonaniya2010/SubDomainizer.git
+cd SubDomainizer
+pip3 install -r requirements.txt
+echo "Complete"
+
 echo "Installing getallurls (gau)"
 GO111MODULE=on go get -u -v github.com/lc/gau
 echo "Complete"
