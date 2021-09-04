c38e5741f06ba1ccd6abd3b9e24056cef8d73887
chrisbenz <cpbenz@protonmail.com>
2021-09-04 15:35:18 -0700

    enhancements, support for subdomainizer, file cleanup

--- ReconCB/reconCB.sh
+++ ReconCB/reconCB.sh
@@ -9,7 +9,6 @@ header() {
 	echo '--------------------------------------------------------------------'
 	echo 'Starting Recon...'
 	echo '--------------------------------------------------------------------'
-
 }
 
 validate() {
@@ -27,7 +26,7 @@ validate() {
 }
 
 subdomains() {
-	cd ${storage}
+	cd $storage
 
 	echo "Grabbing subdomains from (Amass), this may take some time..."
 	amass enum -passive -o amass-enum.txt -r 8.8.8.8 -d ${domain}
@@ -41,22 +40,36 @@ subdomains() {
 	sort -u amass-enum.txt subfinder-enum.txt > domains.txt
 
 	echo '--------------------------------------------------------------------'
-	echo "Checking for live hosts from domains..."
-	cat domains.txt | httprobe > livehosts.txt
-
 	echo "Gather domains from gau..."
-	gau -subs ${domain} | cut -d / -f 3 | sort -u >> livehosts.txt
+	gau -subs ${domain} | cut -d / -f 3 | sort -u >> domains.txt
+
+	echo '--------------------------------------------------------------------'
+	echo "Gathering domains from SubDomainizer..."
+	python3 ~/DownloadedTools/SubDomainizer/SubDomainizer.py -u ${domain} -o sd.txt
+	cat sd.txt >> domains.txt
+
+	sort -u domains.txt > sorteddomains.txt
+
+	echo '--------------------------------------------------------------------'
+	echo "Checking for live hosts from domains..."
+	cat sorteddomains.txt | httprobe > livehosts.txt
 
 	echo "Gathering some screenshots of live hosts..."
-	~/DownloadedTools/EyeWitness/Python/EyeWitness.py -f ~/Desktop/${storage}/livehosts.txt --web -d "${storage}-screenshots"
+	yes n | ~/DownloadedTools/EyeWitness/Python/EyeWitness.py -f ~/Desktop/${storage}/livehosts.txt --web -d screenshots
 
 	echo "Crawling through sites with GoSpider..."
 	gospider -S livehosts.txt -o spiderResults -c 10 -d 1 
 
 	echo "Finished gathering domains, sorting livehosts..."
 	sort livehosts.txt > hostList.txt
-
+	cleanup
 	echo "Done!"
+}
+
+cleanup() {
+	echo "Gathering generated files together..."
+	mkdir utilityFiles
+	mv amass-enum.txt subfinder-enum.txt domains.txt sorteddomains.txt sd.txt livehosts.txt utilityFiles
 }
 
 validate
