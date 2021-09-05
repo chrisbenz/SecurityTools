7da95f17be2825b3bbda27b62873480e851d2a3d
chrisbenz <cpbenz@protonmail.com>
2021-09-04 21:40:46 -0700

    slight script refactor, organizational changes

--- ReconCB/reconCB.sh
+++ ReconCB/reconCB.sh
@@ -3,7 +3,12 @@
 option=$1
 domain=$2
 argCount=$#
-timeStamp=`date +"%Y-%m-%d_%H-%M-%s"`
+timeStamp=`date +"%Y-%m-%d_%s"`
+toolPath=~/DownloadedTools
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
 header() {
 	echo '--------------------------------------------------------------------'
@@ -25,59 +30,78 @@ validate() {
 	fi
 }
 
-subdomains() {
+setup() {
+	storage="${domain}-recon-${timeStamp}"
+	mkdir ${storage}
 	cd $storage
+}
 
-	echo "Grabbing subdomains from (Amass), this may take some time..."
-	amass enum -passive -o amass-enum.txt -r 8.8.8.8 -d ${domain}
+subdomains() {
+	echo "${green}Grabbing subdomains from (Amass), this may take some time..."
+	amass enum -o amass.txt -r 8.8.8.8 -d $domain
 
 	echo '--------------------------------------------------------------------'
 	echo "Grabbing subdomains from (subfinder)..."
-	subfinder -o subfinder-enum.txt -d ${domain}
-
-	echo '--------------------------------------------------------------------'
-	echo "Merging subdomain lists and removing duplicates..."
-	sort -u amass-enum.txt subfinder-enum.txt > domains.txt
+	subfinder -o subfinder.txt -d $domain
 
 	echo '--------------------------------------------------------------------'
 	echo "Gather domains from gau..."
-	gau -subs ${domain} | cut -d / -f 3 | sort -u >> domains.txt
+	gau -subs $domain | cut -d / -f 3 > gau.txt
 
 	echo '--------------------------------------------------------------------'
 	echo "Gathering domains from SubDomainizer..."
-	python3 ~/DownloadedTools/SubDomainizer/SubDomainizer.py -u ${domain} -o sd.txt
-	cat sd.txt >> domains.txt
-
-	sort -u domains.txt > sorteddomains.txt
+	python3 $toolPath/SubDomainizer/SubDomainizer.py -u $domain -o subdomainizer.txt
 
 	echo '--------------------------------------------------------------------'
+	echo "Merging subdomain lists and removing duplicates..."
+	sort -u amass.txt subfinder.txt gau.txt subdomainizer.txt > domains.txt
+
+}
+
+hostStatus() {
+	echo '--------------------------------------------------------------------'
 	echo "Checking for live hosts from domains..."
-	cat sorteddomains.txt | httprobe > livehosts.txt
+	cat domains.txt | httprobe > livehosts.txt
+}
 
-	echo "Gathering some screenshots of live hosts..."
-	yes n | ~/DownloadedTools/EyeWitness/Python/EyeWitness.py -f ~/Desktop/${storage}/livehosts.txt --web -d screenshots
-
+crawl() {
+	echo '--------------------------------------------------------------------'
 	echo "Crawling through sites with GoSpider..."
 	gospider -S livehosts.txt -o spiderResults -c 10 -d 1 
+}
 
-	echo "Finished gathering domains, sorting livehosts..."
-	sort livehosts.txt > hostList.txt
-	cleanup
-	echo "Done!"
+screenshots() {
+	echo '--------------------------------------------------------------------'
+	echo "Gathering some screenshots of live hosts..."
+	yes n | $toolPath/EyeWitness/Python/EyeWitness.py -f livehosts.txt --web -d screenshots
 }
 
 cleanup() {
+	echo '--------------------------------------------------------------------'
 	echo "Gathering generated files together..."
 	mkdir utilityFiles
-	mv amass-enum.txt subfinder-enum.txt domains.txt sorteddomains.txt sd.txt livehosts.txt utilityFiles
+	mv amass.txt subfinder.txt gau.txt subdomainizer.txt domains.txt utilityFiles
+
+	if [ -e geckodriver.log ]
+	then
+		mv geckodriver.log utilityFiles
+	fi
+
+	echo "Finished gathering domains, sorting livehosts..."
+	sort -u livehosts.txt > hostList.txt	
+	echo "Done!${reset}"
 }
 
+#############
 validate
 header
-
-storage="${domain}-recon-${timeStamp}"
-mkdir ${storage}
-
+setup
+#############
 subdomains
+hostStatus
+crawl
+screenshots
+#############
+cleanup
 
 exit 0
