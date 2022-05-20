4c8f8796b166360204e36d77380631cb5c2a3d37
chrisbenz <cpbenz@protonmail.com>
2022-05-19 17:09:10 -0700

    switching argument validation to use argparse

--- ImageRip/imageRip.py
+++ ImageRip/imageRip.py
@@ -1,43 +1,44 @@
+import string
+from tokenize import String
 from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
 from GPSPhoto import gpsphoto
 import exifread
 import sys
+import argparse
 
 def main():
 
-	if len(sys.argv) != 2:
-		print("""Usage: python3 imageRip.py <Image File Name>""")
-		return
+	parser = argparse.ArgumentParser(description='Extract EXIF data from an image')
+	parser.add_argument('imageName', help='Path to an image')
+	args = parser.parse_args()
+	print(args)
 
-	imageName = sys.argv[1]
+	imageName = args.imageName
+
 	try:
 		image = Image.open(imageName)
 	except FileNotFoundError:
 		print(imageName + ' could not be opened. Please specify a valid image path.')
 		return 
 
-	print(image)
 	exifData = image._getexif()
 
 	for tagId in exifData:
-
 		tag = TAGS.get(tagId, tagId)
 		data = exifData.get(tagId)
-
 		if isinstance(data, bytes):
 			try:
 				data = data.decode()
 			except UnicodeError:
 				data = data.decode('latin-1')	
-
 		print(f"{tag:25}: {data}")
 
 	gpsData = gpsphoto.getGPSData(imageName)
 	print(f"{'Latitude':25}: {gpsData['Latitude']}")
 	print(f"{'Longitude':25}: {gpsData['Longitude']}")
 	link = 'https://www.google.com/maps/place/%s,%s' % (gpsData['Latitude'], gpsData['Longitude'])
-	print(f"{'Google Maps Link:25':25}: {link}")
+	print(f"{'Google Maps Link:':25}: {link}")
 
 if __name__ == "__main__":
     main()
